2a3f2f175c6c742652ec3ec55a5d6958873ed36b
PramitBadgami <pramitbadgami2@gmail.com>
2024-05-09 19:18:41 +0545

    Admin Listing

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -14,6 +14,7 @@ export default class Admins extends Component {
         <Sidebar/>
 
         <main className="main-container">
+          <button className='admin-add'>Add Admin</button>
         <table>
             <thead>
                 <tr>
