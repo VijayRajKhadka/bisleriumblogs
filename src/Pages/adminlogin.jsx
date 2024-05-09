3f8084424bcd59883947bdb63819e48813ed8158
PramitBadgami <pramitbadgami2@gmail.com>
2024-05-09 19:22:41 +0545

    Admin Login Complete

--- src/Pages/adminlogin.jsx
+++ src/Pages/adminlogin.jsx
@@ -8,24 +8,25 @@ export default class loginandregister extends Component {
       <div>
       <div className="login-page">
        
-  <div className="form">
-  <h1>Admin Login</h1>
-  <br></br>
-    <form className="register-form">
-      <input type="text" placeholder="name" />
-      <input type="password" placeholder="password" />
-      <input type="text" placeholder="email address" />
-      <button>create</button>
-      <p className="message">Already registered? <a href="#">Sign In</a></p>
-    </form>
-    <form className="login-form">
-      <input type="text" placeholder="username" />
-     
-      <button>login</button>
-      <p className="message">Not registered? <a href="#">Create an account</a></p>
-    </form>
-  </div>
-</div>
+        <div className="form">
+        <h1>Admin Login</h1>
+        <br></br>
+          <form className="register-form">
+            <input type="text" placeholder="name" />
+            <input type="password" placeholder="password" />
+            <input type="text" placeholder="email address" />
+            <button>create</button>
+            <p className="message">Already registered? <a href="#">Sign In</a></p>
+          </form>
+          <form className="login-form">
+            <input type="text" placeholder="username" />
+            <input type="password" placeholder="password" />
+          
+            <button>login</button>
+            <p className="message">Not registered? <a href="#">Create an account</a></p>
+          </form>
+        </div>
+      </div>
 
 
       </div>
