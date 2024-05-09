bebcd7c8d1783604fa34104b7cc74561ba932dad
vijay <vijayrajkhadka100@gmail.com>
2024-05-10 03:45:14 +0545

    Edit and delete button

--- src/Pages/BlogDetails.jsx
+++ src/Pages/BlogDetails.jsx
@@ -10,8 +10,13 @@ import Image1 from "../Assets/Images/login-background.jpg";
 import NavBar from "../Components/NavBar";
 import GlobalService from "../services/GlobalService";
 import { useParams } from "react-router-dom";
-import { commentOnBlog, getAllComments, getBlogDetails } from "../services/BlogServices";
+import {
+  commentOnBlog,
+  getAllComments,
+  getBlogDetails,
+} from "../services/BlogServices";
 import { get } from "firebase/database";
+import { getLocalStorageItem } from "../services/LocalStorageService";
 
 const BlogDetails = () => {
   // const data = props.location.state.data;
@@ -49,30 +54,31 @@ const BlogDetails = () => {
     });
   }, [id]); // Run this effect whenever id changes
 
-
-
-
   const handleComment = () => {
     if (validateComment()) {
-      console.log("🚀 ~ file: BlogDetails.jsx ~ line 120 ~ handleComment ~ comment", comment)
+      console.log(
+        "🚀 ~ file: BlogDetails.jsx ~ line 120 ~ handleComment ~ comment",
+        comment
+      );
       var payload = {
         blogId: id,
         data: {
-          "commentContent": comment,
-          "blogId": id
-        }
-
-      }
+          commentContent: comment,
+          blogId: id,
+        },
+      };
       commentOnBlog(payload).then((res) => {
-        console.log("🚀 ~ file: BlogDetails.jsx ~ line 120 ~ handleComment ~ res", res);
+        console.log(
+          "🚀 ~ file: BlogDetails.jsx ~ line 120 ~ handleComment ~ res",
+          res
+        );
         getAllComments(id).then((res) => {
           setComments(res);
         });
       });
       // console.log("🚀 ~ file: BlogDetails.jsx ~ line 120 ~ handleComment ~ payload", payload)
     }
-
-  }
+  };
 
   return (
     <div>
@@ -94,26 +100,51 @@ const BlogDetails = () => {
                   <p className="user-name">{blogDetails.user.userName}</p>
                   <p className="post-date">3 days ago</p>
                 </div>
-                <div className="saved">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    fill="white"
-                    class="bi bi-bookmark"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z" />
-                  </svg>
-                </div>
+
+                {blogDetails.user.userId == getLocalStorageItem("userId") ? (
+                  <div style={{ display: "flex" }}>
+                    <div className="edit-blog">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        fill="currentColor"
+                        class="bi bi-pencil-square"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                        <path
+                          fill-rule="evenodd"
+                          d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
+                        />
+                      </svg>
+                    </div>
+                    <div className="delete-blog">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        fill="currentColor"
+                        class="bi bi-trash"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+                      </svg>
+                    </div>
+                  </div>
+                ) : (
+                  <div />
+                )}
               </div>
               <div className="card-content">
-                <p>
-                  {blogDetails.content}
-                </p>
-                {
-                  blogDetails.images.length > 0 ? <img src={blogDetails.images[0]} className="image-container"></img> : null
-                }
+                <p>{blogDetails.content}</p>
+                {blogDetails.images.length > 0 ? (
+                  <img
+                    src={blogDetails.images[0]}
+                    className="image-container"
+                  ></img>
+                ) : null}
                 {/* <img src={Image1} className="image-container"></img> */}
                 <br />
                 <div className="post-feed">
@@ -150,7 +181,6 @@ const BlogDetails = () => {
                   <div className="comment-show">
                     {blogDetails.commentCount} comments
                   </div>
-
                 </div>
                 <br />
                 <hr style={{ opacity: "0.22" }} />
@@ -160,34 +190,35 @@ const BlogDetails = () => {
                 <div className="comment-contaiter">
                   <p>Comments</p>
                   <br />
-                  <textarea placeholder="Write a comment" className="comment-input" onChange={(e) => {
-                    setComment(e.target.value);
-                    console.log("🚀 ~ file: BlogDetails.jsx ~ line 191 ~ e.target.value", e.target.value)
-                    validateComment();
-                  }}> </textarea>
-                  <button className="comment-button" onClick={handleComment}>Comment</button>
+                  <textarea
+                    placeholder="Write a comment"
+                    className="comment-input"
+                    onChange={(e) => {
+                      setComment(e.target.value);
+                      console.log(
+                        "🚀 ~ file: BlogDetails.jsx ~ line 191 ~ e.target.value",
+                        e.target.value
+                      );
+                      validateComment();
+                    }}
+                  >
+                    {" "}
+                  </textarea>
+                  <button className="comment-button" onClick={handleComment}>
+                    Comment
+                  </button>
                   <br />
                   <br />
                   <br />
                   <br />
-                  {
-                    comments && comments.map((comment) => {
-                      return <CommentCard comment={comment} />
-                    }
-                    )
-                  }
+                  {comments &&
+                    comments.map((comment) => {
+                      return <CommentCard comment={comment} />;
+                    })}
 
                   <br />
-                  
-
-
-
-
                 </div>
-
               </div>
-
-
             </div>
 
             <div className="user-container">
@@ -197,22 +228,18 @@ const BlogDetails = () => {
                 <p className="blog-user-name">@{blogDetails.user.userName}</p>
                 <br />
               </div>
-              <p className="blog-user-bio"> - {blogDetails.user.firstName} {blogDetails.user.lastName}</p>
+              <p className="blog-user-bio">
+                {" "}
+                - {blogDetails.user.firstName} {blogDetails.user.lastName}
+              </p>
               <p className="blog-user-bio"> - {blogDetails.user.email}</p>
               <p className="blog-user-bio">{blogDetails.user.bio}</p>
-
             </div>
           </div>
         </div>
-
       ) : (
-        <div>
-          Loading...
-        </div>
-      )
-
-      }
-
+        <div>Loading...</div>
+      )}
     </div>
   );
 };
